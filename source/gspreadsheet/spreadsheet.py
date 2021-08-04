04c2c207e84f36e702da3ea69ec7471751896036
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-04 20:31:31 +0300

    class GSpreadsheet is placed in a separate file

--- source/gspreadsheet/spreadsheet.py
+++ source/gspreadsheet/spreadsheet.py
@@ -1,14 +1,3 @@
-from .gservice import GService
-
-
-class GSpreadsheets:
-    def __init__(self, g_service: GService):
-        self.spreadsheets = g_service.spreadsheets()
-
-    def get_spreadsheet(self, spreadsheet_id):
-        return Spreadsheet(spreadsheet_id, self.spreadsheets)
-
-
 class Spreadsheet:
     def __init__(self, spreadsheet_id, spreadsheets):
         self.spreadsheet_id = spreadsheet_id
