495e6fe5a3b3c567f51985110b79228efbdcff2c
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-04 13:27:45 +0300

    created get_table_values method

--- source/gspreadsheet/spreadsheet.py
+++ source/gspreadsheet/spreadsheet.py
@@ -2,5 +2,15 @@ from gservice import GService
 
 
 class Spreadsheet:
-    def __init__(self, g_service: GService):
+    def __init__(self, g_service: GService, spreadsheet_id):
         self.spreadsheets = g_service.spreadsheets()
+        self.spreadsheet_id = spreadsheet_id
+
+    def get_table_values(self, address='A:Z', date_render='SERIAL_NUMBER', dimension='ROWS'):
+        request = self.spreadsheets.values().get(
+            spreadsheetId=self.spreadsheet_id,
+            range=address,
+            dateTimeRenderOption=date_render,
+            majorDimension=dimension
+        )
+        return request.execute()['values']
