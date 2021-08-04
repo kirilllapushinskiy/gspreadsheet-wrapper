e403f781cb97d739de224c4b18bd31b1f23b56e0
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-04 13:12:41 +0300

    created __init__ method

---
+++ source/gspreadsheet/gservice.py
@@ -0,0 +1,12 @@
+from google.oauth2 import service_account
+from googleapiclient import discovery
+
+
+class GService:
+    def __init__(self, service_account_file, scopes):
+        self.credentials = service_account.Credentials.from_service_account_file(
+            service_account_file,
+            scopes=scopes
+        )
+
+        self.service = discovery.build('sheets', 'v4', credentials=self.credentials)
